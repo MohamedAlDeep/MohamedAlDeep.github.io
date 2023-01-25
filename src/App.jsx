5be8dd47fb5e5523e876052454cefbfbe60fa8c8
ICE988 <mohamedsamir22446688@gmail.com>
2023-01-25 14:39:21 +0200

    1.24

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-
-
+import Header from './assets/component/Header/Header'
+import Footer from './assets/component/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     
+      <Header />
+      <Footer />
     </div>
   )
 }
---
+++ src/assets/component/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './css/index.css'
+function Header() {
+  return (
+    <div className='main-div'>
+      <a href="https://github.com/MohamedAlDeep/MohamedAlDeep.github.io">
+        <img  className="github-icon" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github Icon" />
+        <img  className="github-icon" src="https://assets.stickpng.com/images/5847f981cef1014c0b5e48be.png" alt="Git Icon" />
+      </a>
+    </div>
+  )
+}
+
+export default Header
