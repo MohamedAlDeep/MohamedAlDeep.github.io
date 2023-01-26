2ce5aca220c4b6851437f1feb68a090f22f55ff5
ICE988 <mohamedsamir22446688@gmail.com>
2023-01-26 16:12:47 +0200

    header progress

--- src/assets/component/Header/Header.jsx
+++ src/assets/component/Header/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './css/index.css'
+import github from '../../../../public/img/github.png';
+import git from '../../../../public/img/git.png';
+
 function Header() {
   return (
-    <div className='main-div'>
-      <a href="https://github.com/MohamedAlDeep/MohamedAlDeep.github.io">
-        <img  className="github-icon" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github Icon" />
-        <img  className="github-icon" src="https://assets.stickpng.com/images/5847f981cef1014c0b5e48be.png" alt="Git Icon" />
-      </a>
+    <div className="container">
+      <div className='main-div'>
+        <a href="https://github.com/MohamedAlDeep/MohamedAlDeep.github.io">
+          <img width="50px" className="github-icon" src={github} alt="Github Icon" />
+          <img width="50px" className="github-icon" src={git} alt="Git Icon" />
+        </a>
+        <div className='text-container-grid'>
+          <h3>Home</h3>
+          <h3>Projects</h3>
+        </div>
+      </div>
     </div>
   )
 }
